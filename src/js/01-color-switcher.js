83c3ed20777a66f999a82ea71820227a44a36c64
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-22 14:30:22 +0200

    1 задача готово

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,29 @@
 const body = document.querySelector('body');
 const startBtn = document.querySelector("button[data-start]");
 const stopBtn = document.querySelector("button[data-stop]");
+let timerId = null;
+let flag = true;
+stopBtn.setAttribute("disabled", 'true');
+// events
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopButtonClick)
+// functions
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+  
+function onStartBtnClick(e) {
+    stopBtn.removeAttribute("disabled");
+    timerId = setInterval(() => {
+        getRandomHexColor();
+        // let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute("disabled", 'true');
+}
 
-
+function onStopButtonClick(e) {
+    clearInterval(timerId);
+    stopBtn.setAttribute("disabled", 'true');
+    startBtn.removeAttribute("disabled");
+}
