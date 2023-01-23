135d6a6a36c6a2616e54cdb57856b7b808d9b445
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-23 15:18:47 +0200

    js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const baseForm = document.querySelector('.form');
+let position = 0;
+let idInterval = null;
+
+baseForm.addEventListener('submit', valueBaseForm)
+
+function valueBaseForm(e) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = e.currentTarget;
+  intervalStep(Number(delay.value), Number(step.value), Number(amount.value));
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+}
+
+function intervalStep(delay, step, amount) {
+  // delay += step;
+ idInterval = setInterval(() => {
+  position += 1;
+  delay += step;
+  if (position >= amount) {
+    clearInterval(idInterval)
+  }
+  createPromise(position, delay);
+},delay)
 }
